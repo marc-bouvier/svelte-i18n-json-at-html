6b0255ee2df022b0b4ebbc87d7e36884f49db759
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-02-09 16:48:15 +0100

    feat: i18n support

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import '../i18n/index.js';
+	import { _, isLoading } from 'svelte-i18n';
+	import '../app.scss';
+	import { Navigation, NavigationItem } from '@iroco/ui';
+</script>
+{#if !$isLoading}
+	<Navigation
+		navigationItems={[
+      new NavigationItem($_("header.about"), "/about"),
+    ]}
+		type="topbar"
+	/>
+	<main class="main">
+		<slot />
+	</main>
+{/if}
+
+<style lang="scss">
+  @use "node_modules/@iroco/ui/dist/scss/colors.scss";
+  @use "node_modules/@iroco/ui/dist/scss/constants.scss";
+  @import "node_modules/@iroco/ui/dist/scss/containers.scss";
+  @import "node_modules/@iroco/ui/dist/scss/button.scss";
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { _ } from "svelte-i18n";
+</script>
+
+<svelte:head>
+	<title>{$_("page.title")}</title>
+</svelte:head>
+<section>
+<h1>{$_("temp.lorem")}</h1>
+<p>{$_("temp.lipsum")}</p>
+</section>
+
+<style lang="scss">
+  @use "node_modules/@iroco/ui/dist/scss/colors";
+  @import "node_modules/@iroco/ui/dist/scss/button";
+</style>
