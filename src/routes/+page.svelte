c2a5ddffecf97aaf29ac76d93307cc1cbb64dc8b
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-02-22 15:49:52 +0100

    feat: articles with images showcase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,64 @@
 <script lang="ts">
 	import { _ } from 'svelte-i18n';
+	import ImageArticle from '$iroco-ui/ImageArticle.svelte';
 </script>
 
 <svelte:head>
 	<title>{$_('page.title')}</title>
 </svelte:head>
-<section>
+
+<section class="page-content">
 	<h1>Use</h1>
 	<p>{$_('temp.lipsum')}</p>
-
+	<ImageArticle
+		articleTitle="Lorem Ipsum"
+		alt="alt"
+		articleContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut porttitor leo a diam. Gravida dictum fusce ut placerat orci nulla. Pulvinar sapien et ligula ullamcorper. Ultrices neque ornare aenean euismod. Facilisi cras fermentum odio eu feugiat pretium."
+		imgSrc="https://placehold.co/600x400"
+		buttonList={[
+			{href:"#",label:"Label"},
+			{href:"#",label:"Label"},
+			]}
+	></ImageArticle>
+	<ImageArticle
+		articleTitle="Lorem Ipsum"
+		alt="alt"
+		articleContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut porttitor leo a diam. Gravida dictum fusce ut placerat orci nulla. Pulvinar sapien et ligula ullamcorper. Ultrices neque ornare aenean euismod. Facilisi cras fermentum odio eu feugiat pretium."
+		imgSrc="https://placehold.co/600x400"
+		buttonList={[
+			{href:"#",label:"Label"},
+			{href:"#",label:"Label"},
+			]}
+		reversed
+	></ImageArticle>
+	<ImageArticle
+		articleTitle="Lorem Ipsum"
+		alt="alt"
+		articleContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut porttitor leo a diam. Gravida dictum fusce ut placerat orci nulla. Pulvinar sapien et ligula ullamcorper. Ultrices neque ornare aenean euismod. Facilisi cras fermentum odio eu feugiat pretium."
+		imgSrc="https://placehold.co/600x400"
+		buttonList={[
+			{href:"#",label:"Label"},
+			{href:"#",label:"Label"},
+			]}
+	></ImageArticle>
+	<ImageArticle
+		articleTitle="Lorem Ipsum"
+		alt="alt"
+		articleContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut porttitor leo a diam. Gravida dictum fusce ut placerat orci nulla. Pulvinar sapien et ligula ullamcorper. Ultrices neque ornare aenean euismod. Facilisi cras fermentum odio eu feugiat pretium."
+		imgSrc="https://placehold.co/600x400"
+		buttonList={[
+			{href:"#",label:"Label"},
+			{href:"#",label:"Label"},
+			]}
+		reversed
+	></ImageArticle>
 </section>
 
 <style lang="scss">
+
+  .page-content {
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+  }
 </style>
