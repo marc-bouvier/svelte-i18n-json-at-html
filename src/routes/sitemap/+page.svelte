fe4750f52d405c59aaec94ef13045288cd0bc87e
Bruno Thomas <bruno@barreverte.fr>
2024-04-12 17:10:06 +0200

    feat: add Project/Values/Sitemap for demo

---
+++ src/routes/sitemap/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+import { _ } from 'svelte-i18n';
+import { sitemap } from '../../sitemap';
+
+</script>
+
+<svelte:head>
+	<title>{$_('sitemap.title')}</title>
+</svelte:head>
+
+<div class="sitemap">
+	<section class="sitemap">
+		<h1>{$_('sitemap.title')}</h1>
+		<ul>
+		{#each Object.keys(sitemap) as page}
+				<li><a href={page}>page</a></li>
+		{/each}
+		</ul>
+	</section>
+</div>
