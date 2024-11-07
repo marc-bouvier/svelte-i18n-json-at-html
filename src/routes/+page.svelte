bc4924ff0b191d3fb971cdcfe90a7e6f9cc4f980
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-11-07 16:19:22 +0100

    feat: hero layout with border along the whole content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,99 +5,100 @@
 <svelte:head>
 	<title>{$_('page.title')}</title>
 </svelte:head>
-
-	<section class="hero">
-		<div class="hero-content">
-			<h1>Vous guider vers un numérique responsable</h1>
-			<p>
-				Nous sommes convaincus que l’urgence écologique et sociale doit nous mener à transformer nos modèles économiques
-				et sociétaux.
-			</p>
-		</div>
-		<img class="illustration" src="img/Rosace-Rezofora-RVB.svg" alt />
-	</section>
-	<section class="cards">
-		<div class="card">
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
-				tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
-				malesuada interdum. Phasellus tristique ac leo at fringilla.
-			</p>
-			<button>Je découvre</button>
-		</div>
-		<div class="card">
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
-				tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
-				malesuada interdum. Phasellus tristique ac leo at fringilla. Cras a viverra nibh, vitae rutrum ante. Sed
-				pharetra
-				nibh ac velit dignissim ornare sed accumsan lacus.
-				lectus.
-			</p>
-			<button>Je découvre</button>
-		</div>
-		<div class="card">
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
-				tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
-				malesuada interdum. Phasellus tristique ac leo at fringilla.
-			</p>
-			<button>Je découvre</button>
-		</div>
-	</section>
-	<section class="title-and-text as-tertiary">
+<section class="hero">
+	<div class="hero-content">
 		<header>
-			<h2>Comment l’intégrer dans votre entreprise ?</h2>
+			<h1>Vous accompagner dans la compréhension de vos enjeux de durabilité et le pilotage de votre transition</h1>
 		</header>
+		<p>
+			Notre mission : vous guider dans la transition environnementale et sociale de votre organisation grâce à un
+			accompagnement adapté — conseil, formation, inspiration — basé sur une approche pédagogique et bienveillante.
+		</p>
+	</div>
+
+</section>
+<section class="cards">
+	<div class="card">
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
+			tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
+			malesuada interdum. Phasellus tristique ac leo at fringilla.
+		</p>
+		<button>Je découvre</button>
+	</div>
+	<div class="card">
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
 			tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
 			malesuada interdum. Phasellus tristique ac leo at fringilla. Cras a viverra nibh, vitae rutrum ante. Sed
 			pharetra
+			nibh ac velit dignissim ornare sed accumsan lacus.
+			lectus.
 		</p>
-		<p>
-			nibh ac velit dignissim ornare sed accumsan lacus. Vestibulum tincidunt purus sapien, ac dapibus sem semper a.
-			Nullam venenatis vestibulum risus id molestie. Aliquam facilisis nunc at nunc aliquam hendrerit. Etiam sodales
-			sodales lectus, ut suscipit lacus vehicula nec. Proin vel magna sed orci condimentum ornare in a odio. In varius
-			eu
-			augue eget tincidunt. Ut quis porttitor nulla. Nulla leo nunc, efficitur non ipsum vel, tincidunt auctor lectus.
-		</p>
-	</section>
-
-	<section class="title-and-text as-primary">
-		<header>
-			<h2>Comment l’intégrer dans votre entreprise ?</h2>
-		</header>
+		<button>Je découvre</button>
+	</div>
+	<div class="card">
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
 			tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
-			malesuada interdum. Phasellus tristique ac leo at fringilla. Cras a viverra nibh, vitae rutrum ante. Sed
-			pharetra
+			malesuada interdum. Phasellus tristique ac leo at fringilla.
 		</p>
-		<p>
-			nibh ac velit dignissim ornare sed accumsan lacus. Vestibulum tincidunt purus sapien, ac dapibus sem semper a.
-			Nullam venenatis vestibulum risus id molestie. Aliquam facilisis nunc at nunc aliquam hendrerit. Etiam sodales
-			sodales lectus, ut suscipit lacus vehicula nec. Proin vel magna sed orci condimentum ornare in a odio. In varius
-			eu
-			augue eget tincidunt. Ut quis porttitor nulla. Nulla leo nunc, efficitur non ipsum vel, tincidunt auctor lectus.
-		</p>
-	</section>
+		<button>Je découvre</button>
+	</div>
+</section>
+<section class="title-and-text as-tertiary">
+	<header>
+		<h2>Comment l’intégrer dans votre entreprise ?</h2>
+	</header>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
+		tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
+		malesuada interdum. Phasellus tristique ac leo at fringilla. Cras a viverra nibh, vitae rutrum ante. Sed
+		pharetra
+	</p>
+	<p>
+		nibh ac velit dignissim ornare sed accumsan lacus. Vestibulum tincidunt purus sapien, ac dapibus sem semper a.
+		Nullam venenatis vestibulum risus id molestie. Aliquam facilisis nunc at nunc aliquam hendrerit. Etiam sodales
+		sodales lectus, ut suscipit lacus vehicula nec. Proin vel magna sed orci condimentum ornare in a odio. In varius
+		eu
+		augue eget tincidunt. Ut quis porttitor nulla. Nulla leo nunc, efficitur non ipsum vel, tincidunt auctor lectus.
+	</p>
+</section>
 
-	<section class="title-and-text as-secondary">
-		<header>
-			<h2>Comment l’intégrer dans votre entreprise ?</h2>
-		</header>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
-			tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
-			malesuada interdum. Phasellus tristique ac leo at fringilla. Cras a viverra nibh, vitae rutrum ante. Sed
-			pharetra
-		</p>
-		<p>
-			nibh ac velit dignissim ornare sed accumsan lacus. Vestibulum tincidunt purus sapien, ac dapibus sem semper a.
-			Nullam venenatis vestibulum risus id molestie. Aliquam facilisis nunc at nunc aliquam hendrerit. Etiam sodales
-			sodales lectus, ut suscipit lacus vehicula nec. Proin vel magna sed orci condimentum ornare in a odio. In varius
-			eu
-			augue eget tincidunt. Ut quis porttitor nulla. Nulla leo nunc, efficitur non ipsum vel, tincidunt auctor lectus.
-		</p>
-	</section>
+<section class="title-and-text as-primary">
+	<header>
+		<h2>Comment l’intégrer dans votre entreprise ?</h2>
+	</header>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
+		tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
+		malesuada interdum. Phasellus tristique ac leo at fringilla. Cras a viverra nibh, vitae rutrum ante. Sed
+		pharetra
+	</p>
+	<p>
+		nibh ac velit dignissim ornare sed accumsan lacus. Vestibulum tincidunt purus sapien, ac dapibus sem semper a.
+		Nullam venenatis vestibulum risus id molestie. Aliquam facilisis nunc at nunc aliquam hendrerit. Etiam sodales
+		sodales lectus, ut suscipit lacus vehicula nec. Proin vel magna sed orci condimentum ornare in a odio. In varius
+		eu
+		augue eget tincidunt. Ut quis porttitor nulla. Nulla leo nunc, efficitur non ipsum vel, tincidunt auctor lectus.
+	</p>
+</section>
+
+<section class="title-and-text as-secondary">
+	<header>
+		<h2>Comment l’intégrer dans votre entreprise ?</h2>
+	</header>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros lacus, commodo eu tristique non, ultricies eu
+		tellus. Nulla facilisi. Integer a tincidunt purus. Proin vulputate tristique magna. Aliquam id eros id ante
+		malesuada interdum. Phasellus tristique ac leo at fringilla. Cras a viverra nibh, vitae rutrum ante. Sed
+		pharetra
+	</p>
+	<p>
+		nibh ac velit dignissim ornare sed accumsan lacus. Vestibulum tincidunt purus sapien, ac dapibus sem semper a.
+		Nullam venenatis vestibulum risus id molestie. Aliquam facilisis nunc at nunc aliquam hendrerit. Etiam sodales
+		sodales lectus, ut suscipit lacus vehicula nec. Proin vel magna sed orci condimentum ornare in a odio. In varius
+		eu
+		augue eget tincidunt. Ut quis porttitor nulla. Nulla leo nunc, efficitur non ipsum vel, tincidunt auctor lectus.
+	</p>
+</section>
