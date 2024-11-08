dffe063315e7acad13142bb1399437aa606307cd
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-11-08 12:45:42 +0100

    feat: Elise Martin and Adeline Agut picture for team members

--- src/lib/TeamMember.svelte
+++ src/lib/TeamMember.svelte
@@ -4,25 +4,24 @@
 	export let bio: string = '';
 	export let picture: string = '';
 </script>
+
 <li class="team-member">
-	<img src="{picture}" style="border-radius: 50%;margin: 2rem	" />
-	<p style="font-weight: bold;margin: .125rem">{ name }</p>
-	<p style="font-weight: bold;margin: .125rem">{ topic }</p>
-	<p style="margin: .125rem">{ bio }</p>
+	<img class="member-picture" src={picture} style="border-radius: 50%;margin: 2rem	" />
+	<p style="font-weight: bold;margin: .125rem">{name}</p>
+	<p style="font-weight: bold;margin: .125rem">{topic}</p>
+	<p style="margin: .125rem">{bio}</p>
 </li>
 
-<style type="text/css">
-    .team-member {
-        /*border: 1px solid;*/
-        border-radius: 0.25rem;
-        display: flex;
-        flex-direction: column;
-        position: relative;
+<style type="text/scss">
+	.team-member {
+		/*border: 1px solid;*/
+		border-radius: 0.25rem;
+		display: flex;
+		flex-direction: column;
+		position: relative;
+	}
 
-    }
-
-		.team-member p{
-        color: var(--White);
-		}
-
+	.team-member p {
+		color: var(--White);
+	}
 </style>
