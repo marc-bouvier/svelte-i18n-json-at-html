e88f7a197d95cdd7db0d17df33fda3df740b0f7d
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-11-14 22:12:39 +0100

    build: upgrade dependencies

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import prettier from 'eslint-config-prettier';
+import svelte from 'eslint-plugin-svelte';
+
+export default [
+	prettier,
+	...svelte.configs['flat/prettier'],
+	{
+		ignores: [
+			'node_modules',
+			'/build',
+			'/.svelte-kit',
+			'/package',
+			'.env',
+			'.env.*',
+			'!.env.example',
+			'pnpm-lock.yaml',
+			'package-lock.json',
+			'yarn.lock'
+		]
+	}
+];
