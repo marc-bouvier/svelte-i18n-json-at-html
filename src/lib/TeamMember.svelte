9e78a5442e1fe168abe106b1dc0b5e7e67044dbe
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-11-14 12:35:33 +0100

    a11y: blank alt text for members pictures because their name ar close to the picture

--- src/lib/TeamMember.svelte
+++ src/lib/TeamMember.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <li class="team-member">
-	<img class="member-picture" src={picture} style="border-radius: 50%;margin: 2rem	" />
+	<img class="member-picture" src={picture} alt="" style="border-radius: 50%;margin: 2rem	" />
 	<p style="font-weight: bold;margin: .125rem">{name}</p>
 	<p style="font-weight: bold;margin: .125rem">{topic}</p>
 	<p style="margin: .125rem">{bio}</p>
